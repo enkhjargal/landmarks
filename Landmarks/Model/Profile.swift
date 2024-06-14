73303482dd7ddfa6efcc107141d15d9b82985439
enkhjargal <enkhjargal.dev@gmail.com>
2024-06-14 22:44:29 +0900

    Working with UI controls

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Enkhjargal on 14/06/2024.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "enkhjargal")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
